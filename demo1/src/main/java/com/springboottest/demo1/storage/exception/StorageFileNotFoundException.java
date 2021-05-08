46448c74c032e74e7840ab0dfdc772d4fd1d756a
ftoorto <m.f.d.y.n.a@live.com>
2021-05-08 18:17:38 +0800

    javaweb_demo1

---
+++ demo1/src/main/java/com/springboottest/demo1/storage/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.springboottest.demo1.storage.exception;
+@SuppressWarnings("serial")
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
