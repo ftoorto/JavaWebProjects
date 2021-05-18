f0f1702bc260fd9b082d5166032922c9fa103f89
ftoorto <m.f.d.y.n.a@live.com>
2021-05-19 01:11:38 +0800

    pack2

---
+++ pack2/src/main/java/com/springboottest/pack2/Pack2Application.java
@@ -0,0 +1,13 @@
+package com.springboottest.pack2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pack2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pack2Application.class, args);
+    }
+
+}
---
+++ pack2/src/test/java/com/springboottest/pack2/Pack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottest.pack2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pack2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
